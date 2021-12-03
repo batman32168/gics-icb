f84398ddf31538303b6ee94839768356f91264bb
batman32168 <batamn32168@web.de>
2021-12-03 22:43:49 +0100

    If no valid code - on same level - found we will use the parent code. resolve #1

--- src/convert.py
+++ src/convert.py
@@ -4,19 +4,23 @@ from src.mapping.gics2icb import mapping as gics_mapping
 class Convert:
 
     @staticmethod
-    def gcis_to_icb(gics: Taxonomy):
+    def gics_to_icb(gics: Taxonomy):
         code =gics.code
-        icb_code = gics_mapping.get(code)
-        if icb_code is not None:
-            icb_classification = Taxonomy(icb_code,'ICB',gics.year)
-            description ="";
-            current_level = icb_classification.current_level
-            definition =  icb_classification.level(current_level)
-            if current_level ==1 or current_level ==4:
-                description = definition['description']
-            return {
-                'code': icb_classification.code,
-                'name': definition['name'],
-                'description': description
-            }
+        if len(code) >= 2:
+            icb_code = gics_mapping.get(code)
+            if icb_code is not None and icb_code != '':
+                icb_classification = Taxonomy(icb_code,'ICB',gics.year)
+                description ="";
+                current_level = icb_classification.current_level
+                definition =  icb_classification.level(current_level)
+                if current_level ==1 or current_level ==4:
+                    description = definition['description']
+                return {
+                    'code': icb_classification.code,
+                    'name': definition['name'],
+                    'description': description
+                }
+            else:
+                new_gics = Taxonomy(code[:-2], 'GICS', '2018')
+                return Convert.gics_to_icb(new_gics)
         return None
--- test/TestConvert.py
+++ test/TestConvert.py
@@ -5,12 +5,28 @@ from src.convert import Convert
 class TestConvert(unittest.TestCase):
     def test_convert_first_level(self):
         valid_GICS = Taxonomy('10','GICS','2018')
-
-        valid_ICB = Convert.gcis_to_icb(valid_GICS)
+        valid_ICB = Convert.gics_to_icb(valid_GICS)
         self.assertIsNotNone(valid_ICB)
         self.assertTrue(valid_GICS.is_valid)
         self.assertIs(valid_ICB["code"], "60")
         self.assertIs(valid_ICB["name"], "Energy")
+
+    def test_convert_second_level(self):
+        valid_GICS = Taxonomy('1010', 'GICS', '2018')
+        valid_ICB = Convert.gics_to_icb(valid_GICS)
+        self.assertIsNotNone(valid_ICB)
+        self.assertTrue(valid_GICS.is_valid)
+        self.assertIs(valid_ICB["code"], "6010")
+        self.assertIs(valid_ICB["name"], "Energy")
+
+    def test_convert_second_level_with_parent(self):
+        valid_GICS = Taxonomy('1510', 'GICS', '2018')
+        valid_ICB = Convert.gics_to_icb(valid_GICS)
+        self.assertIsNotNone(valid_ICB)
+        self.assertTrue(valid_GICS.is_valid)
+        self.assertIs(valid_ICB["code"], "55")
+        self.assertIs(valid_ICB["name"], "Basic Materials")
+
 
 if __name__ == '__main__':
     unittest.main()
